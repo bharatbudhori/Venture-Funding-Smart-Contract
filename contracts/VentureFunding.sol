5659ac3acd2127ef7be5c56945b61d9b274d4a75
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2023-10-07 10:04:50 +0530

    first commit

---
+++ contracts/VentureFunding.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract VentureFunding {
+    constructor() {}
+
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    // function createCampaign();
+
+    
+}
