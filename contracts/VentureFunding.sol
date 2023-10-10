70aac9dbcad5c44a82012552199ec0686c2c1c89
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2023-10-10 10:00:39 +0530

    Second commit

--- contracts/VentureFunding.sol
+++ contracts/VentureFunding.sol
@@ -20,7 +20,55 @@ contract VentureFunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    // function createCampaign();
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
 
-    
+        require(campaign.deadline < block.timestamp, "Deadline must be in the future");
+        
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.image = _image;
+        campaign.amountCollected = 0;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+        
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        require(campaign.deadline < block.timestamp, "Deadline must be in the future");
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent) = payable(campaign.owner).send(amount);
+
+        if(sent) {
+            campaign.amountCollected += amount;
+        }
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory){
+        Campaign[] memory _campaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint256 i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage campaign = campaigns[i];
+            _campaigns[i] = campaign;
+        }
+
+        return _campaigns;
+
+    }
 }
